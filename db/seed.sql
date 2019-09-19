090e5f9929cf4ef37129d344700e87f28b33b9b5
iuglyboi28 <iuglyboi28@gmail.com>
2019-09-19 15:50:55 -0400

    did my sql

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+-- insert burgers
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Baconzilla', false, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Veggie Burger', false, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Krabby Patty', false, CURRENT_TIMESTAMP);
