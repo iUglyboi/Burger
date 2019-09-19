48d475105a05dd872016052e67ad4d850e2a4c0d
iuglyboi28 <iuglyboi28@gmail.com>
2019-09-19 15:59:08 -0400

    did my config and module exports based of past activities.

---
+++ config/orm.js
@@ -0,0 +1,31 @@
+const connnection = require('/.connection.js');
+
+var orm = {
+    chooseAll: function (callback) {
+        connection.query('SELECT * FROM burgers', function (err, result) {
+            if (err) throw err;
+            callback(result);
+        });
+    },
+    putOne: function (burger_name, callback) {
+        connection.query('INSERT INTO burgers SET ?',
+            {
+                burger_name: burger_name,
+                devoured: false,
+            }, function (err, result) {
+                if (err) throw err;
+                callback(result);
+            });
+    },
+    updateOne: function (burgerID, callback) {
+        connection.query('UPDATE burgers SET ? WHERE ?', [{devoured: true}, {id: burgerID}],
+            function(err, result){
+                if (err) throw err;
+                callback(result);
+            })
+    }
+
+    }
+
+    module.exports = orm;
+    
