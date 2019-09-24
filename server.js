5dd2ea0bb082c73bc2c321b856c89effb27a766f
iuglyboi28 <iuglyboi28@gmail.com>
2019-09-24 18:19:03 -0400

    idk bs

--- server.js
+++ server.js
@@ -13,13 +13,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
+
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+
 app.set("view engine", "handlebars");
 
 
-var routes = require("./controllers/burgers_controller");
+var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
